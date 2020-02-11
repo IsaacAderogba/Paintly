1be99775991652973d4644ff7817df017fd6045d
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-02-11 07:41:33 -0800

    Refactor swatches component

--- src/components/molecules/Swatches.tsx
+++ src/components/molecules/Swatches.tsx
@@ -12,6 +12,45 @@ import DataAttributesEnum from "../../~reusables/constants/dataAttributes";
 // styles
 import { styled, ThemeContext } from "../../~reusables/contexts/ThemeContext";
 
+const colorSwatches: { swatch: DataAttributesEnum }[] = [
+  {
+    swatch: DataAttributesEnum.white
+  },
+  {
+    swatch: DataAttributesEnum.black
+  },
+  {
+    swatch: DataAttributesEnum.red
+  },
+  {
+    swatch: DataAttributesEnum.green
+  },
+  {
+    swatch: DataAttributesEnum.blue
+  },
+  {
+    swatch: DataAttributesEnum.teal
+  },
+  {
+    swatch: DataAttributesEnum.purple
+  },
+  {
+    swatch: DataAttributesEnum.yellow
+  },
+  {
+    swatch: DataAttributesEnum.sand
+  },
+  {
+    swatch: DataAttributesEnum.orange
+  },
+  {
+    swatch: DataAttributesEnum.lightblue
+  },
+  {
+    swatch: DataAttributesEnum.pink
+  }
+];
+
 const Swatches: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   className
 }) => {
@@ -22,270 +61,30 @@ const Swatches: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
 
   return (
     <StyledSwatches className={className}>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.white
-            }
-          })
-        }
-        data-color={DataAttributesEnum.white}
-        css={css`
-          background-color: ${DataAttributesEnum.white === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
+      {colorSwatches.map(({ swatch }) => (
         <div
+          onClick={() =>
+            dispatch({
+              type: EditorActType.UPDATE_CANVAS_TOOL,
+              payload: {
+                swatch: swatch
+              }
+            })
+          }
+          data-color={swatch}
           css={css`
-            background-color: ${DataAttributesEnum.white};
+            background-color: ${swatch === state.swatch
+              ? lightBackground
+              : "#ffffff"};
           `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.black
-            }
-          })
-        }
-        data-color={DataAttributesEnum.black}
-        css={css`
-          background-color: ${DataAttributesEnum.black === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.black};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.red
-            }
-          })
-        }
-        data-color={DataAttributesEnum.red}
-        css={css`
-          background-color: ${DataAttributesEnum.red === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.red};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.green
-            }
-          })
-        }
-        data-color={DataAttributesEnum.green}
-        css={css`
-          background-color: ${DataAttributesEnum.green === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.green};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.blue
-            }
-          })
-        }
-        data-color={DataAttributesEnum.blue}
-        css={css`
-          background-color: ${DataAttributesEnum.blue === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.blue};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.teal
-            }
-          })
-        }
-        data-color={DataAttributesEnum.teal}
-        css={css`
-          background-color: ${DataAttributesEnum.teal === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.teal};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.purple
-            }
-          })
-        }
-        data-color={DataAttributesEnum.purple}
-        css={css`
-          background-color: ${DataAttributesEnum.purple === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.purple};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.yellow
-            }
-          })
-        }
-        data-color={DataAttributesEnum.yellow}
-        css={css`
-          background-color: ${DataAttributesEnum.yellow === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.yellow};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.sand
-            }
-          })
-        }
-        data-color={DataAttributesEnum.sand}
-        css={css`
-          background-color: ${DataAttributesEnum.sand === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.sand};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.orange
-            }
-          })
-        }
-        data-color={DataAttributesEnum.orange}
-        css={css`
-          background-color: ${DataAttributesEnum.orange === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.orange};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.lightblue
-            }
-          })
-        }
-        data-color={DataAttributesEnum.lightblue}
-        css={css`
-          background-color: ${DataAttributesEnum.lightblue === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.lightblue};
-          `}
-        />
-      </div>
-      <div
-        onClick={() =>
-          dispatch({
-            type: EditorActType.UPDATE_CANVAS_TOOL,
-            payload: {
-              swatch: DataAttributesEnum.pink
-            }
-          })
-        }
-        data-color={DataAttributesEnum.pink}
-        css={css`
-          background-color: ${DataAttributesEnum.pink === state.swatch
-            ? lightBackground
-            : "#ffffff"};
-        `}
-      >
-        <div
-          css={css`
-            background-color: ${DataAttributesEnum.pink};
-          `}
-        />
-      </div>
+        >
+          <div
+            css={css`
+              background-color: ${swatch};
+            `}
+          />
+        </div>
+      ))}
     </StyledSwatches>
   );
 };
